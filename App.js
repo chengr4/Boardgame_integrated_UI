2408f300ee42390d4eaf3e0f6f8a67f452dfa20c
Eddie Cheng <local@example.test>
2020-07-29 16:41:21 +0200

    installed icon library

--- App.js
+++ App.js
@@ -4,10 +4,11 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-// import each GUI (Home, Youtube, Forum)
+// import each GUI (Home, Games, Youtube, Forum)
 import {Home} from './GUI/Home';
 import {YTChannel} from './GUI/YTChannel/YTChannel';
 import {Forum} from './GUI/Forum';
+import {Games} from './GUI/Games';
 
 
 const Tab = createBottomTabNavigator();
@@ -17,6 +18,7 @@ export default function App() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Games" component={Games} />
         <Tab.Screen name="Youtube" component={YTChannel} />
         <Tab.Screen name="Forum" component={Forum} />
       </Tab.Navigator>
--- GUI/YTChannel/YTChannel.js
+++ GUI/YTChannel/YTChannel.js
@@ -1,12 +1,34 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
-import {Search} from './Search';
+import { Text, View, StyleSheet } from 'react-native';
+import {Card} from './Card';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 
+// watch https://www.youtube.com/watch?v=LdKtugH-sb8 
 export function YTChannel() {
+  // load Icon library
+  Icon.loadFont();
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Search />
+    <View>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Youtube</Text>
+        <Icon name="search" size={32}/>
+      </View>
+    <Card />
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  header:{
+    marginTop: 50,
+    height: 60,
+    backgroundColor: 'skyblue',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  headerText:{
+    fontSize: 30,
+    textAlign: 'center',
+  },
+});
