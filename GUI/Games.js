10d44dd4b6ecfdca7f50fdd705c3e800a2ed484a
Eddie Cheng <local@example.test>
2020-07-29 16:42:20 +0200

    added Game UI.js (beginning)

---
+++ GUI/Games.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+// https://medium.com/swlh/react-native-youtube-ui-1a6f4799ac27 
+//(use this link to make UI)
+export function Games() {
+  return (
+    <View>
+      <View style={styles.header}>
+
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+header:{
+  marginTop: 50,
+  height: 60,
+  backgroundColor: 'skyblue',
+},
+});
