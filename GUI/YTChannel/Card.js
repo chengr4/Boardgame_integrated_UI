81b969353365920348749a7b7d7bf69b1a75eece
Eddie Cheng <local@example.test>
2020-07-30 16:55:15 +0200

    finished basic youtube GUI

--- GUI/YTChannel/Card.js
+++ GUI/YTChannel/Card.js
@@ -1,24 +1,41 @@
 import * as React from 'react';
 import { Text, View, StyleSheet, Image } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import data from './test_data.json';
 
-export function Card() {
+export function Card({currentVideo}) {
   return (
     <View style={styles.card}>
       <Image 
-        source={{uri:'https://images.pexels.com/photos/1763075/pexels-photo-1763075.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'}}
+        source={{uri:currentVideo.snippet.thumbnails.medium.url}}
         style={{
           width:'100%',
-          height:200
+          height:200,
         }}
       />
+      <View style={styles.describeCard}>
+        {/* here may have a profile */}
+        <View style={styles.videoDetail}>
+          <Text style={styles.videoTitle}>{currentVideo.snippet.title}</Text>
+          <Text style={styles.videoState}>
+            {currentVideo.snippet.channelTitle + " " + currentVideo.statistics.viewCount}
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   card: {
-    flex:1,
+    padding: 15,
   },
-  
+  describeCard: {
+  },
+  videoDetail: {
+    paddingHorizontal: 15,
+  },
+  videoTitle: {
+    fontSize: 16
+  },
+
 });
--- GUI/YTChannel/YTChannel.js
+++ GUI/YTChannel/YTChannel.js
@@ -1,34 +1,46 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 import {Card} from './Card';
-import Icon from 'react-native-vector-icons/Ionicons';
-
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import data from './test_data.json';
 
 // watch https://www.youtube.com/watch?v=LdKtugH-sb8 
 export function YTChannel() {
-  // load Icon library
-  Icon.loadFont();
   return (
-    <View>
+    <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerText}>Youtube</Text>
-        <Icon name="search" size={32}/>
+          <TouchableOpacity style={styles.icon}>
+            <Icon name="search" size={32}/>
+          </TouchableOpacity>
       </View>
-    <Card />
+      <View style={styles.body}>
+        <FlatList
+           data = {data.items}
+           renderItem = {({item})=><Card currentVideo={item} />}
+         />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   header:{
     marginTop: 50,
     height: 60,
     backgroundColor: 'skyblue',
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    alignItems: 'center',
+    justifyContent: 'space-between', // space between yt and search icon
   },
   headerText:{
     fontSize: 30,
     textAlign: 'center',
   },
+  icon: {
+    padding: 15,
+  }
 });
